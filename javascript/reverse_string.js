90cb547449d841aefd3530581d7cfe27da2d7777
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-10-09 14:56:08 -0600

    pseudocode

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -15,5 +15,17 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
+// pseudocode
+/**
+ * given a string,
+ * take the last element of the string
+ * add it as the first element of a new string
+ * repeat until done
+ */
 // And a written explanation of your solution
+/**
+ * reversing strings is a simple (and solved, mostly) problem that requires some index
+ * manipulation. there are efficiency problems that can be addressed by using the same string
+ * or by slicing and replacing without having to look at the values of the string at all,
+ * but these are not required to solve the issue
+ */
